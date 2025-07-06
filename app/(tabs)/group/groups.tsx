37f5b751ee3fe00315536d57ac915b6163cf28ad
rayner <raynergoh03@gmail.com>
2025-07-06 19:24:52 +0800

    icon in groups page

--- app/(tabs)/group/groups.tsx
+++ app/(tabs)/group/groups.tsx
@@ -44,9 +44,9 @@ export default function GroupsScreen() {
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={
         <IconSymbol
-          size={310}
+          size={350}
           color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
+          name="person.3.sequence.fill"
           style={styles.headerImage}
         />
       }>
